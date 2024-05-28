1d308fcb5b9f904ab9f96bb7f8738f36f1d61c02
josueramirez1 <josue67ramirez@gmail.com>
2024-05-28 10:39:24 -0700

    adding files

---
+++ app/script.js
@@ -0,0 +1,20 @@
+const hello = "hello world";
+
+function createHello() {}
+
+const hello2 = () => {
+  const hello = "today is a good day";
+};
+
+// Are the comments working here?
+
+function newFun() {
+  const yo = 223;
+}
+
+// Are we going somewhere today to
+
+let ff = `${hello}`;
+
+let me = "Josue";
+function working() {}
